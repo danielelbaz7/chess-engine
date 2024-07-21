aad53482f256b1c557b71eb9d286f10747ab765f
daniel <175679070+danielelbaz7@users.noreply.github.com>
2024-07-21 16:17:14 -0400

    deleted piece modifier boards

--- src/Board.java
+++ src/Board.java
@@ -42,50 +42,93 @@ public class Board {
     }
 
     //initializes the bitboards
-    private void createBitboards(int[][] bitboards)
-    {
+    private void createBitboards(int[][] bitboards) {
         //runs the creator for each individual board
-        for(int i = 0; i < 12; i++)
-        {
+        for (int i = 0; i < 12; i++) {
             //logs the current board
             int[] currentBoard = new int[120];
             //base board without any logged pieces
             StringBuilder baseLong = new StringBuilder
                     ("222222222222222222222000000002200000000220000000022000000002200000000220000000022000000002200000000222222222222222222222");
-            for(int j = 0; j < 120; j++)
-            {
-                String currentPiece = boardTemplate[j/10][j%10];
+            for (int j = 0; j < 120; j++) {
+                String currentPiece = boardTemplate[j / 10][j % 10];
 
-                if(i == 0) { if (currentPiece.equals("r")) { baseLong.replace(j, j + 1, "1"); } }
-                if(i == 1) { if (currentPiece.equals("n")) { baseLong.replace(j, j + 1, "1"); } }
-                if(i == 2) { if (currentPiece.equals("b")) { baseLong.replace(j, j + 1, "1"); } }
-                if(i == 3) { if (currentPiece.equals("q")) { baseLong.replace(j, j + 1, "1"); } }
-                if(i == 4) { if (currentPiece.equals("k")) { baseLong.replace(j, j + 1, "1"); kingLocations[0] = j; } }
-                if(i == 5) { if (currentPiece.equals("p")) { baseLong.replace(j, j + 1, "1"); } }
-                if(i == 6) { if (currentPiece.equals("R")) { baseLong.replace(j, j + 1, "1"); } }
-                if(i == 7) { if (currentPiece.equals("N")) { baseLong.replace(j, j + 1, "1"); } }
-                if(i == 8) { if (currentPiece.equals("B")) { baseLong.replace(j, j + 1, "1"); } }
-                if(i == 9) { if (currentPiece.equals("Q")) { baseLong.replace(j, j + 1, "1"); } }
-                if(i == 10) { if (currentPiece.equals("K")) { baseLong.replace(j, j + 1, "1"); kingLocations[1] = j; } }
-                if(i == 11) { if (currentPiece.equals("P")) { baseLong.replace(j, j + 1, "1"); } }
+                if (i == 0) {
+                    if (currentPiece.equals("r")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
+                if (i == 1) {
+                    if (currentPiece.equals("n")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
+                if (i == 2) {
+                    if (currentPiece.equals("b")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
+                if (i == 3) {
+                    if (currentPiece.equals("q")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
+                if (i == 4) {
+                    if (currentPiece.equals("k")) {
+                        baseLong.replace(j, j + 1, "1");
+                        kingLocations[0] = j;
+                    }
+                }
+                if (i == 5) {
+                    if (currentPiece.equals("p")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
+                if (i == 6) {
+                    if (currentPiece.equals("R")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
+                if (i == 7) {
+                    if (currentPiece.equals("N")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
+                if (i == 8) {
+                    if (currentPiece.equals("B")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
+                if (i == 9) {
+                    if (currentPiece.equals("Q")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
+                if (i == 10) {
+                    if (currentPiece.equals("K")) {
+                        baseLong.replace(j, j + 1, "1");
+                        kingLocations[1] = j;
+                    }
+                }
+                if (i == 11) {
+                    if (currentPiece.equals("P")) {
+                        baseLong.replace(j, j + 1, "1");
+                    }
+                }
 
             }
-            for(int k = 0; k < 120; k++)
-            {
-                currentBoard[k] = Integer.parseInt(baseLong.substring(k, k+1));
+            for (int k = 0; k < 120; k++) {
+                currentBoard[k] = Integer.parseInt(baseLong.substring(k, k + 1));
             }
             bitboards[i] = currentBoard;
         }
     }
 
     //sets the boardlist to the newly generated boards and prints the board to the console
-    public void generateBoards()
-    {
+    public void generateBoards() {
         createBitboards(pieceBoards);
-        for(int i = 0; i < 120; i++)
-        {
-            if(i%10 == 0)
-            {
+        for (int i = 0; i < 120; i++) {
+            if (i % 10 == 0) {
                 System.out.println();
             }
             System.out.print(pieceBoards[5][i]);
@@ -93,15 +136,14 @@ public class Board {
     }
 
     //gui usage
-    public boolean isThereAPieceThere(int rowpass, int colpass)
-    {
+    public boolean isThereAPieceThere(int rowpass, int colpass) {
         int startingIndex = 0;
-        if(Boolean.TRUE.equals(whiteTurn)) {
+        if (Boolean.TRUE.equals(whiteTurn)) {
             startingIndex = 6;
         }
         int pieceIndex = (((rowpass * 8) + 20) + (colpass + 1)) + (rowpass * 2);
-        for(int i = startingIndex; i < startingIndex+6; i++) {
-            if(pieceBoards[i][pieceIndex] == 1) {
+        for (int i = startingIndex; i < startingIndex + 6; i++) {
+            if (pieceBoards[i][pieceIndex] == 1) {
                 return true;
             }
         }
@@ -115,7 +157,7 @@ public class Board {
         int otherSide = side == 1 ? 0 : 1;
         HashMap<Integer, Integer> totalPossibleMoves = new HashMap<>();
         //looks through enemy pieces and finds their possible moves
-        for (int i = otherSide * 6; i < otherSide*6 + 6; i++) {
+        for (int i = otherSide * 6; i < otherSide * 6 + 6; i++) {
             for (int j = 0; j < bitboards[0].length; j++) {
                 if (bitboards[i][j] == 1) {
                     totalPossibleMoves.putAll(vm.possibleMoveFinderAllPieces(j, bitboards));
@@ -130,8 +172,8 @@ public class Board {
         //sets score to 1000 or -1000 if in check mate
         double totalScore = 0;
         int sideToCheck = isWhiteTurn ? 1 : 0;
-        if(isKingChecked(pieceBoards, sideToCheck, kingLocations)) {
-            if(vm.allAvailableMoves(pieceBoards, sideToCheck).isEmpty()) {
+        if (isKingChecked(pieceBoards, sideToCheck, kingLocations)) {
+            if (vm.allAvailableMoves(pieceBoards, sideToCheck).isEmpty()) {
                 if (sideToCheck == 1) {
                     totalScore = -1000;
                     return totalScore;
@@ -142,7 +184,7 @@ public class Board {
             }
         }
         //adds or removes based on distance and piece value
-        for(int i = 0; i < 12; i++) {
+        for (int i = 0; i < 12; i++) {
             int baseValue = switch (i) {
                 case 0 -> -5;
                 case 1, 2 -> -3;
@@ -156,32 +198,33 @@ public class Board {
             };
             double negativeOrPositive = baseValue > 0 ? 1 : -1;
             //runs through each bitboard, checks if there is a piece there
-            for(int j = 0; j < bitboards[i].length; j++) {
-                if(bitboards[i][j] == 1) {
+            for (int j = 0; j < bitboards[i].length; j++) {
+                if (bitboards[i][j] == 1) {
                     //if there is a piece there, find the value and find its distance from the center and multiply that by the base value
-                    double centerDistance = Math.abs(Conv.to64From120(j)%8 - 3.5);
+
+                    /*double centerDistance = Math.abs(Conv.to64From120(j)%8 - 3.5);
                     double heightDistance = Math.abs(Conv.to64From120(j)/8 - 3.5);
-                    totalScore += (baseValue / (centerDistance * heightDistance))/4;
+                    totalScore += (baseValue / (centerDistance * heightDistance))/4;*/
 
 
                 }
             }
         }
 
-        return (double) Math.round(totalScore * 100) /100;
+        return (double) Math.round(totalScore * 100) / 100;
     }
 
     //takes a location and set of boards to find the current bitboard of a piece
     private static int getBaseValue(int location, int[][] bitboards) {
         int boardWithPiece = -1;
 
-        for(int i = 0; i < 12; i++) {
-            if(bitboards[i][location] == 1) {
+        for (int i = 0; i < 12; i++) {
+            if (bitboards[i][location] == 1) {
                 boardWithPiece = i;
             }
         }
 
-        return switch(boardWithPiece) {
+        return switch (boardWithPiece) {
             case 0 -> -5;
             case 1, 2 -> -3;
             case 3 -> -9;
@@ -194,8 +237,7 @@ public class Board {
         };
     }
 
-    public static String[][] getBoardTemplate()
-    {
+    public static String[][] getBoardTemplate() {
         return boardTemplate;
     }
 
