86c767760f1ae3e010d9be0fc25fa18e219d8d7d
175679070+danielelbaz7@users.noreply.github.com <danielelbazcode@gmail.com>
2024-07-23 02:17:55 -0400

    iskingchecked

--- src/BoardMethods.java
+++ src/BoardMethods.java
@@ -16,4 +16,18 @@ public class BoardMethods {
         return false;
     }
 
+    public static boolean isKingChecked(int[][] bitboards, int[] kingLocations, int side) {
+        int otherSide = side == 1 ? 0 : 1;
+        MoveSet totalPossibleMoves = new MoveSet();
+        //looks through enemy pieces and finds their possible moves
+        for (int i = otherSide * 6; i < otherSide * 6 + 6; i++) {
+            for (int j = 0; j < bitboards[0].length; j++) {
+                if (bitboards[i][j] == 1) {
+                    totalPossibleMoves.addAll(ValidMoves.possibleMoveFinderAllPieces(j, bitboards, i));
+                }
+            }
+        }
+        return totalPossibleMoves.containsMove(kingLocations[side]);
+    }
+
 }
