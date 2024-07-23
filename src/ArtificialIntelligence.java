bee90bfa20e26c14dfe6af4e12079eb26a7ba110
175679070+danielelbaz7@users.noreply.github.com <danielelbazcode@gmail.com>
2024-07-23 03:06:30 -0400

    wip on minimax

--- src/ArtificialIntelligence.java
+++ src/ArtificialIntelligence.java
@@ -4,7 +4,7 @@ import java.util.Map;
 public class ArtificialIntelligence {
 
     //locates best move based on tree of moves
-    public double findBestMove(Board b, int depth, boolean ourTurn) {
+    public static double findBestMove(Board b, int depth, boolean ourTurn) {
 
         if(depth == 0 || BoardMethods.isKingCheckmated(b.pieceBoards, b.kingLocations, 0) || BoardMethods.isKingCheckmated(b.pieceBoards, b.kingLocations, 1)) {
             return BoardMethods.evaluateBoard(b.pieceBoards, b.kingLocations, true);
