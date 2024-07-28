7c492d574a31e35c5d26d447725995b8248c0311
175679070+danielelbaz7@users.noreply.github.com <danielelbazcode@gmail.com>
2024-07-28 01:37:51 -0400

    final product w/out tests

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,6 @@ public class Main {
         //generates a board and a gui based on it
         Board gameBoard = new Board();
         ChessGUI chessGUI = new ChessGUI(gameBoard);
-        long time = -System.currentTimeMillis();
-        long ops = -ArtificialIntelligence.countOfOps;
-        GameplayController.allowMovement = false;
-        System.out.println(ArtificialIntelligence.findBestMove(gameBoard, 1, Integer.MIN_VALUE, Integer.MAX_VALUE, gameBoard.whiteTurn).getEval());
-        GameplayController.allowMovement = true;
-        time += System.currentTimeMillis();
-        ops += ArtificialIntelligence.countOfOps;
-        System.out.println((double) time / 1000 + " seconds, " + ops + " total operations. " + (1000 * ops) / (time) + " operations a second.");
 
     }
 }
